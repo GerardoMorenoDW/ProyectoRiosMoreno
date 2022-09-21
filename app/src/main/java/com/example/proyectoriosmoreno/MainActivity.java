4433fe6c0e4400bbb06c8d56000423057a988a65
GerardoMorenoDW <gdmg507@gmail.com>
2022-09-21 04:52:03 -0500

    Votación en las elecciones + login

--- app/src/main/java/com/example/proyectoriosmoreno/MainActivity.java
+++ app/src/main/java/com/example/proyectoriosmoreno/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.proyectoriosmoreno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText cedula;
+    Button botonlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cedula = findViewById(R.id.plaincedula);
+        botonlogin = findViewById(R.id.buttonlogin);
+
+        botonlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (cedula.getText().toString().equals("")){
+
+                }else{
+                    Toast.makeText(MainActivity.this, "Cedula incorrecta o ya voto", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
